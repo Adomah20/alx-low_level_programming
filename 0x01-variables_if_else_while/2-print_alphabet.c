0b5c6ac80087b616537087646a230fcd77b87c4f
Adomah20 <adomahbernard20@gmail.com>
2022-10-16 11:30:24 -0700

    printting of alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main-program entry point.
+ * Return:0 if no error, non zero if error.
+ **/
+int main(void)
+{
+char c;
+for (c = 'a'; c < = 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
