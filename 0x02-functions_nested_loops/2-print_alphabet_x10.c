ccb6fe8d4612cf4c367bdcf6632d6da8eb09ca2f
Adomah20 <adomahbernard20@gmail.com>
2022-10-18 15:57:28 -0700

    10 timesa alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times alphabet in small letter
+ */
+void print_alphabet_x10(void)
+{
+int counter = 0;
+char c;
+while (counter++ <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
