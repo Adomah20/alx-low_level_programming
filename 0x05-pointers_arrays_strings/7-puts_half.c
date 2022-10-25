a6a07f10fa183488716115c43badf03f312a0513
Adomah20 <adomahbernard20@gmail.com>
2022-10-25 13:34:56 -0700

    printing half of a sting

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - printing half of a string
+ * @str: character to be parsed
+ * Return: o always
+ */
+
+void puts_half(char *str)
+{
+	int s, t;
+
+	s = 0;
+
+	while (str[s] != '\n')
+		s++;
+	if (s + 1 % 2 != '0')
+		t = (s - 1) / 2;
+	else
+		t = (s / 2);
+	t++;
+
+	for (s = t; str[s] != '\0'; s++)
+	_putchar(str[s]);
+	_putchar('\n');
+}
