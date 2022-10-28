eb1d86f8d3bc824090ba3429e8a585e3af67f7a1
Adomah20 <adomahbernard20@gmail.com>
2022-10-28 05:49:52 -0700

    encoding strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13
+ * @s: the string
+ * Return: string
+ */
+
+char *rot13(char *s)
+{
+	int x, y;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (x = 0; *(s + x); x++)
+	{
+		for (y = 0; y < 52; y++)
+		{
+			if (a[y] == *(s + x))
+			{
+				*(s + x) = b[y];
+				break;
+			}
+		}
+	}
+	return (s);
+}
