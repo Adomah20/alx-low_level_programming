207a1d1b155f47f8091242f0972e1f7a314a6336
Adomah20 <adomahbernard20@gmail.com>
2022-11-01 18:11:10 -0700

    function that searches a string for any set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - function that searches a string for any set of bytes
+ * @s: first string
+ * @accept: matches one of the bytes
+ * Return: a pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	while (*s)
+	{
+		for (a = 0; accept[a]; a++)
+		{
+			if (*s == accept[a])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
