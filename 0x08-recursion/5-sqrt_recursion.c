cf41545616f9f45d0d893dcabf297737d0fc9fb5
Adomah20 <adomahbernard20@gmail.com>
2022-11-03 20:40:45 -0700

    square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * check - function that returns the natural sqaure root of a number
+ * @a: number
+ * @b: number
+ * Return: integer
+ */
+
+int check(int a, int b)
+{
+	if (a * a == b)
+	{
+		return (a);
+	}
+	if (a * a > b)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (check(a + 1, b));
+	}
+}
