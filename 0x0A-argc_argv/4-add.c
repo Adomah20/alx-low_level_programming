526b5951cc6aa98c6f42d48380c4dd9c562082c6
Adomah20 <adomahbernard20@gmail.com>
2022-11-04 19:25:41 -0700

    program to add two positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that add two positive numbers
+ * @argc: argument
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int a, b, sum = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (a = 1; argv[a]; a++)
+	{
+		b = strtol(argv[a], &flag, 10);
+		if (&flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum = sum + b;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
